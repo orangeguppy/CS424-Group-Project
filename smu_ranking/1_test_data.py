54c6a75d5d121187a1aaee8a480b2a5183a815f8
jesleenkaurtakhar <jesleen123@gmail.com>
2024-02-25 14:46:59 +0800

    created rnn model

---
+++ smu_ranking/1_test_data.py
@@ -0,0 +1,14 @@
+#testing
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        del images, labels, outputs
+
+    print('Accuracy of the network on the {} test images: {} %'.format(10000, 100 * correct / total))   
