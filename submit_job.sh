13e68086960bb6c0513ab3382f95c84c324134cb
orangeguppy <vuwet467@gmail.com>
2024-03-10 15:46:22 +0800

    Config for GPU

--- submit_job.sh
+++ submit_job.sh
@@ -12,12 +12,12 @@ SBATCH --cpus-per-task=4           # Number of CPU to request for the job
 SBATCH --mem=8GB                   # How much memory does your job require?
 SBATCH --gres=gpu:1                # Do you require GPUS? If not delete this line
 SBATCH --time=01-00:00:00          # How long to run the job for? Jobs exceed this time will be terminated
-                                    Format <DD-HH:MM:SS> eg. 5 days 05-00:00:00
-                                    Format <DD-HH:MM:SS> eg. 24 hours 1-00:00:00 or 24:00:00
+                                    # Format <DD-HH:MM:SS> eg. 5 days 05-00:00:00
+                                    # Format <DD-HH:MM:SS> eg. 24 hours 1-00:00:00 or 24:00:00
 SBATCH --mail-type=BEGIN,END,FAIL  # When should you receive an email?
 SBATCH --output=%u.%j.out          # Where should the log files go?
-                                    You must provide an absolute path eg /common/home/module/username/
-                                    If no paths are provided, the output file will be placed in your current working directory
+                                    # You must provide an absolute path eg /common/home/module/username/
+                                    # If no paths are provided, the output file will be placed in your current working directory
 
 ################################################################
 ## EDIT AFTER THIS LINE IF YOU ARE OKAY WITH DEFAULT SETTINGS ##
