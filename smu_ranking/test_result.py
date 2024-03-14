cbca4d292eabdf821da0fd50d7dfac5659d84da2
saltydeath <kwekzhiyi2511@gmail.com>
2024-03-14 21:57:24 +0800

    theoretically should work

--- smu_ranking/test_result.py
+++ smu_ranking/test_result.py
@@ -3,49 +3,66 @@ from torchvision.datasets import ImageFolder
 import torch.nn.functional as F
 import os
 import cv2
+import torch.nn as nn
+from torchvision import models
+from torch import optim
+import densenet201
+import os
+from PIL import Image
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
 
-def create_test_res(img_dir, device, model, classes):
-    # Path to the folder containing images
-    folder_path = img_dir
+class ImageDataset(Dataset):
+    def init(self, image_dir, transform):
+        self.image_dir = image_dir
+        self.image_paths = os.listdir(image_dir)
+        self.transform = transform
 
-    # List to store images
-    images = []
+    def len(self):
+        return len(self.image_paths)
 
-    # Loop through all files in the folder
-    for filename in os.listdir(folder_path):
-        # Read the image
-        image_path = os.path.join(folder_path, filename)
-        img = cv2.imread(image_path)
-        if img is not None:
-            # Append the image to the list
-            images.append(img)
+    def getitem(self, idx):
+        image_path = os.path.join(self.image_dir, self.image_paths[idx])
+        image = Image.open(image_path).convert("RGB")
+        image = self.transform(image)
+        return image
 
-    non_smu_index = classes.index("non smu")
-    print('non smu index: {non_smu_index}')
+# Define preprocessing transformations
+preprocess = transforms.Compose([
+    transforms.Resize(224, 224),
+    # transforms.CenterCrop(224),  # Assuming DenseNet input size is 224x224
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    #create text file
-    f = open("id_est.txt", "w")
+# Create custom DataLoader
+image_dir = "../eval_simulator/image_folder"
+image_names = os.listdir(image_dir)
+image_dataset = ImageDataset(image_dir, transform=preprocess)
+data_loader = DataLoader(image_dataset, batch_size=16, shuffle=False, num_workers=4)
+#load model
+model = densenet201.DenseNet201(num_classes=35).to(device)
+PATH = './densenet.pth'
+model.load_state_dict(torch.load(PATH))
 
-    #do the predictions
+#get non smu index
+non_smu_index = 17
+
+#create text file
+f = open("id_est.txt", "w")
+# Iterate through the DataLoader and make predictions
+model.eval()
+i = 0
+for batch in data_loader:
     with torch.no_grad():
-        correct = 0
-        total = 0
-        i = 0
-        for images, labels in test_loader:
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            print(outputs.data)
-            #gives highest prob, predicted class
-            _, predict = torch.max(outputs.data, 1)
-            #list of probabilities for each 
-            probabilities = F.softmax(outputs.data, 1)
-            for p in probabilities:
-                likelihood_smu = 1- p[non_smu_index]
-                #return this and add to text file
-            res = zip(_, predict)
-            del images, labels, outputs
-    
-    print('Accuracy of the network on the {} test images: {} %'.format(len(test_loader), 100 * correct / total))
-    f.close()
-    
+        output = model(batch)
+
+    # Post-process predictions as needed
+    probabilities = torch.nn.functional.softmax(output, dim=1)
+    predicted_classes = torch.argmax(probabilities, dim=1)
+    for p in probabilities:
+        likelihood_smu = 1- p[non_smu_index]
+        f.write(image_names[i] + ' '+ likelihood_smu + '\n')
+        i += 1
